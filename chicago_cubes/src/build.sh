d2bf64b8561f161de89e7fa56325aede60e1e1b1
Conor Cary <conor.cary@gmail.com>
2025-02-19 20:01:14 -0500

    Silence OpenGL deprecation warnings

--- chicago_cubes/src/build.sh
+++ chicago_cubes/src/build.sh
@@ -5,7 +5,7 @@ if [ ! -d ../build ]; then mkdir ../build; fi
 pushd ../build > /dev/null
 
 
-clang ../src/sdl_main.cpp -o sdl_main -framework OpenGL -framework SDL2 -F /Library/Frameworks/
+clang ../src/sdl_main.cpp -o sdl_main -DGL_SILENCE_DEPRECATION -framework OpenGL -framework SDL2 -F /Library/Frameworks/
 
 
 popd > /dev/null
